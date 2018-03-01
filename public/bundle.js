7e8af4374d559470a249e4f7f37dfffc1b17883c
zhgqiang <z397435052@163.com>
2018-03-01 09:31:57 +0800

    1

--- public/bundle.js
+++ public/bundle.js
@@ -46012,81 +46012,105 @@ function LensFlare() {
 "use strict";
 
 
+var _render = __webpack_require__(2);
+
+var _render2 = _interopRequireDefault(_render);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var renderer = (0, _render2.default)();
+document.querySelector("#container").appendChild(renderer.domElement);
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = undefined;
+
 var _three = __webpack_require__(0);
 
-var _threeOrbitcontrols = __webpack_require__(2);
+var _threeOrbitcontrols = __webpack_require__(3);
 
 var _threeOrbitcontrols2 = _interopRequireDefault(_threeOrbitcontrols);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-/**
+var Renderer = function Renderer() {
+    /**
      * 创建场景对象
      */
-var scene = new _three.Scene();
-/**
- * 创建网格模型
- */
-var box = new _three.BoxGeometry(100, 100, 100); //创建一个立方体几何对象
-var material = new _three.MeshLambertMaterial({ color: 0x00ff00 }); //材质对象
-var mesh = new _three.Mesh(box, material); //网格模型对象
-scene.add(mesh);
+    var scene = new _three.Scene();
+    /**
+     * 创建网格模型
+     */
+    var box = new _three.BoxGeometry(100, 100, 100); //创建一个立方体几何对象
+    var material = new _three.MeshLambertMaterial({ color: 0x00ff00 }); //材质对象
+    var mesh = new _three.Mesh(box, material); //网格模型对象
+    scene.add(mesh);
 
-//点光源
-var point = new _three.PointLight(0xffffff);
-point.position.set(400, 200, 300); //点光源位置
-scene.add(point); //点光源添加到场景中
-//环境光
-var ambient = new _three.AmbientLight(0x444444);
-scene.add(ambient);
-/**
- * 相机设置
- */
-var width = window.innerWidth; //窗口宽度
-var height = window.innerHeight; //窗口高度
-var k = width / height; //窗口宽高比
-var s = 100; //三维场景缩放系数
-//创建相机对象
-var camera = new _three.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
-camera.position.set(250, 300, 200); //设置相机位置
-camera.lookAt(scene.position); //设置相机方向(指向的场景对象)
-/**
- * 创建渲染器对象
- */
-var renderer = new _three.WebGLRenderer();
-renderer.setSize(width, height);
-renderer.setClearColor(0xb9d3ff, 1); //设置背景颜色
-// document.body.appendChild(renderer.domElement);//body元素中插入canvas对象
+    //点光源
+    var point = new _three.PointLight(0xffffff);
+    point.position.set(400, 200, 300); //点光源位置
+    scene.add(point); //点光源添加到场景中
+    //环境光
+    var ambient = new _three.AmbientLight(0x444444);
+    scene.add(ambient);
+    /**
+     * 相机设置
+     */
+    var width = window.innerWidth; //窗口宽度
+    var height = window.innerHeight; //窗口高度
+    var k = width / height; //窗口宽高比
+    var s = 100; //三维场景缩放系数
+    //创建相机对象
+    var camera = new _three.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
+    camera.position.set(250, 300, 200); //设置相机位置
+    camera.lookAt(scene.position); //设置相机方向(指向的场景对象)
+    /**
+     * 创建渲染器对象
+     */
+    var renderer = new _three.WebGLRenderer();
+    renderer.setSize(width, height);
+    renderer.setClearColor(0xb9d3ff, 1); //设置背景颜色
+    // document.body.appendChild(renderer.domElement);//body元素中插入canvas对象
 
-//执行渲染操作
-renderer.render(scene, camera);
-var T0 = new Date(); //上次时间
-// function render() {
-//     let T1 = new Date();//本次时间
-//     let t = T1-T0;//时间差
-//     T0 = T1;//把本次时间赋值给上次时间
-//     requestAnimationFrame(render);
-//     renderer.render(scene,camera);//执行渲染操作
-//     mesh.rotateY(0.001*t);//旋转角速度0.001弧度每毫秒
-// }
-// render();
+    //执行渲染操作
+    renderer.render(scene, camera);
+    var T0 = new Date(); //上次时间
+    // function render() {
+    //     let T1 = new Date();//本次时间
+    //     let t = T1-T0;//时间差
+    //     T0 = T1;//把本次时间赋值给上次时间
+    //     requestAnimationFrame(render);
+    //     renderer.render(scene,camera);//执行渲染操作
+    //     mesh.rotateY(0.001*t);//旋转角速度0.001弧度每毫秒
+    // }
+    // render();
 
-function render() {
-  renderer.render(scene, camera); //执行渲染操作
-  mesh.rotateY(0.01); //每次绕y轴旋转0.01弧度
-  requestAnimationFrame(render); //请求再次执行渲染函数render
-}
-render();
-var controls = new _threeOrbitcontrols2.default(camera); //创建控件对象
-// controls.addEventListener('change', render);//监听鼠标、键盘事件
+    function render() {
+        renderer.render(scene, camera); //执行渲染操作
+        mesh.rotateY(0.01); //每次绕y轴旋转0.01弧度
+        requestAnimationFrame(render); //请求再次执行渲染函数render
+    }
+    render();
+    var controls = new _threeOrbitcontrols2.default(camera); //创建控件对象
+    // controls.addEventListener('change', render);//监听鼠标、键盘事件
 
-// const greeter = require('./Greeter.js');
-//
+    // const greeter = require('./Greeter.js');
+    console.log(renderer.domElement);
+    return renderer;
+};
 
-document.querySelector("#container").appendChild(renderer.domElement);
+exports.default = Renderer;
 
 /***/ }),
-/* 2 */
+/* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var THREE = __webpack_require__(0)
